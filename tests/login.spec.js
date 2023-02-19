72329ec8f7a720d1c62b5d9432841f07f8420e1a
Paphanan Saowichit <111863991+paphananSa@users.noreply.github.com>
2023-02-19 17:30:15 +0700

    add new case
    add new case  login

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,15 +1,29 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('login tool QA', async ({ page }) => {
+test('login Tools QA correct', async ({ page }) => {
   await page.goto('https://demoqa.com/login');
   
   await page.getByPlaceholder('UserName').fill('demoqa');
   await page.getByPlaceholder('Password').fill('Welcome1!');
   await page.getByRole('button', { name: 'Login' }).click();
 
-  await expect('id=userName-value').toContainText('demoqa');
-  // await page.close();
-  // Expects the URL to contain intro.
-  // await expect(page).toHaveURL(/.*intro/);
+  // Expects correct UserName
+  await expect(page.locator('id=userName-value')).toContainText("demoqa");
+  // const username_text = await page.locator('id=userName-value');
+  // console.log(username_text);
+  await page.getByRole('button', { name: 'Log out' }).click();
+  await page.close();
+});
+
+test('login Tools QA incorrect', async ({ page }) => {
+  await page.goto('https://demoqa.com/login');
+  
+  await page.getByPlaceholder('UserName').fill('demoqa');
+  await page.getByPlaceholder('Password').fill('Welcome');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  // Expects validate Invalid username or password
+  await expect(page.getByText('Invalid username or password!')).toContainText("Invalid username or password!");
+  await page.close();
 });
